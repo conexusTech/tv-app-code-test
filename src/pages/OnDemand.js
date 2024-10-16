19899522b263af85f3de56d098ca22ce0a14a63a
danielconexus <155175960+danielconexus@users.noreply.github.com>
2024-10-16 12:38:02 -0400

    Add OnDemand page

---
+++ src/pages/OnDemand.js
@@ -0,0 +1,14 @@
+// src/pages/OnDemand.js
+
+import React from 'react';
+
+// OnDemand page placeholder
+function OnDemand() {
+  return (
+    <div className="OnDemand">
+      <h2>On Demand Content Coming Soon</h2>
+    </div>
+  );
+}
+
+export default OnDemand;
