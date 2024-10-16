67f4e63293265e7c1e60627b678ae82ebd47321f
danielconexus <155175960+danielconexus@users.noreply.github.com>
2024-10-16 12:39:28 -0400

    Add Settings page

---
+++ src/pages/Settings.js
@@ -0,0 +1,15 @@
+// src/pages/Settings.js
+
+import React from 'react';
+
+// Settings page component
+function Settings() {
+  return (
+    <div className="Settings">
+      <h2>Settings</h2>
+      <p>Settings page content goes here.</p>
+    </div>
+  );
+}
+
+export default Settings;
