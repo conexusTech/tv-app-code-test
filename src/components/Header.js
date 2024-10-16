8f7e0199fbdae441032f4bdaffaf60207ab875b4
danielconexus <155175960+danielconexus@users.noreply.github.com>
2024-10-16 12:07:12 -0400

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+// src/components/Header.js
+
+import React from 'react';
+
+// Header component displaying the app's title
+function Header() {
+  return (
+    <header className="Header">
+      <h1>Simple TV App</h1>
+    </header>
+  );
+}
+
+export default Header;
