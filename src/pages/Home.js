1531dac7a1b52e3cecc6cee05e0e4bd29dd94484
danielconexus <155175960+danielconexus@users.noreply.github.com>
2024-10-16 12:37:10 -0400

    Add Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+// src/pages/Home.js
+
+import React from 'react';
+
+// Home page component welcoming users
+function Home() {
+  return (
+    <div className="Home">
+      <h2>Welcome to the TV App Home Page</h2>
+      <p>Select an option from the menu to get started.</p>
+    </div>
+  );
+}
+
+export default Home;
