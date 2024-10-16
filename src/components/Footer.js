6ab4014f9bcf4f8324a999abbfec7698d826e286
danielconexus <155175960+danielconexus@users.noreply.github.com>
2024-10-16 12:08:08 -0400

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+// src/components/Footer.js
+
+import React from 'react';
+
+// Footer component displaying footer information
+function Footer() {
+  return (
+    <footer className="Footer">
+      <p>&copy; 2023 TV App</p>
+    </footer>
+  );
+}
+
+export default Footer;
